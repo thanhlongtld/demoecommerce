f2ed396abaa151e46ad54b184d44ae15df995ebc
Long Do <soi.ink.finance@gmail.com>
2022-02-28 21:24:44 +0700

    update

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
 
 # Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'shop/index.html', context)
